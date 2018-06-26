8c029b330f5b27e2058d2f4de6bb4a9b5455c0a8
Sahil Chaudhry <schaudhry@deloitte.com>
2018-06-26 10:28:25 -0600

    code cleanup

--- aura/GoogleAddressComp/GoogleAddressCompController.js
+++ aura/GoogleAddressComp/GoogleAddressCompController.js
@@ -2,18 +2,12 @@
     buttonPress : function(component, event, helper) {
         //Generate URL for request to Google APIs
         var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + component.get('v.address') + '&key=AIzaSyDHgwYiOsSK5Ag2F_ID5kuAAj-bfwcEGqI';
-        //var metaWeatherUrl = 'https://www.metaweather.com/api/location/search/?query=' + component.get('v.address');
-        //var metaWeatherUrl = 'https://www.metaweather.com/api/location/search/?lattlong=36.96,-122.02';
-
-        //Add API key if provided
-        // if(!$A.util.isUndefined(component.get('v.apikey'))){
-        //     url += '&key=' + component.get('v.apikey');
-        //
-        // }
-
-        //Make Ajax request
         helper.makeAjaxRequest(component, url);
-        //helper.makeAjaxRequest(component, metaWeatherUrl);
-
-    }
+    },
+    keyCheck: function(component, event, helper){
+        if(event.which == 13) {
+            var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + component.get('v.address') + '&key=AIzaSyDHgwYiOsSK5Ag2F_ID5kuAAj-bfwcEGqI';
+            helper.makeAjaxRequest(component, url);
+        }
+    },
 })
--- aura/GoogleAddressComp/GoogleAddressCompHelper.js
+++ aura/GoogleAddressComp/GoogleAddressCompHelper.js
@@ -5,17 +5,8 @@
         //Make Ajax request by calling method from utils component
         utils.callAjax("POST", url, true,
             function(xmlhttp){
-                console.log('xmlhttp:', xmlhttp);
-
-                //Show response text if successful
-                //Display error message otherwise
                 if (xmlhttp.status == 200) {
-                    component.set('v.msg', xmlhttp.responseText);
-                    component.set('v.msgSeverity', 'information');
-                    component.set('v.msgTitle', 'Success');
-
                     var resultFromGoogle = JSON.parse(xmlhttp.responseText);
-
                     if(resultFromGoogle) {
                         var latitude = resultFromGoogle.results[0].geometry.location.lat;
                         var longitude = resultFromGoogle.results[0].geometry.location.lng;
@@ -25,21 +16,10 @@
                             long : longitude
                         });
                         callMetaWeatherEvt.fire();
-                        console.log('event fired!');
                     }
-
-
+                } else {
+                        console.log('error !');
                 }
-                else if (xmlhttp.status == 400) {
-                    component.set('v.msg', 'There was an error 400');
-                    component.set('v.msgSeverity', 'error');
-                    component.set('v.msgTitle', 'Error');
-                }else {
-                    component.set('v.msg', 'Something else other than 200 was returned');
-                    component.set('v.msgSeverity', 'error');
-                    component.set('v.msgTitle', 'Error');
-                }
-            }
-        );
-    }
+            });
+    },
 })
